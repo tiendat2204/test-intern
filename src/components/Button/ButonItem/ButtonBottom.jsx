b22ee9f6d56bf2b0bfe1adf485d268e9aa0baf27
tiendat2204 <121950007+tiendat2204@users.noreply.github.com>
2024-09-14 18:52:43 +0700

    fix some button

--- src/components/Button/ButonItem/ButtonBottom.jsx
+++ src/components/Button/ButonItem/ButtonBottom.jsx
@@ -1,36 +1,72 @@
 import { useState } from "react";
-import Button from "../../../ui/Button";
 import Checkbox from "../../../ui/Checkbox";
-
+import Button from "../../../ui/Button";
 const ButtonBottom = () => {
   const [checked, setChecked] = useState(false);
- 
+
   return (
-    <div className="w-full p-4 flex justify-center space-x-4">
-      <div className="flex items-center justify-between flex-col">
-        <label
-          htmlFor="weuiAgreeCheckbox"
-          className="flex items-center space-x-2"
-        > 
-         <Checkbox id="weuiAgreeCheckbox" checked={checked} onChange={setChecked} />
-          <span className="text-sm text-gray-600">
-            Đọc và đồng ý với{" "}
-            <a
-              href="/example/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-green-500 hover:underline"
-            >
-              Điều khoản liên quan
-            </a>
-          </span>
-        </label>{" "}
-        
-        <div className="flex space-x-4 mt-5">
-          <Button className="bg-green-500 text-white hover:bg-green-600 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-            Đọc và đồng ý
-          </Button>
-          <Button className="bg-gray-300 text-gray-700">Hủy bỏ</Button>
+    <div className="flex flex-col h-screen bg-white">
+      <div className="flex-grow overflow-auto p-4">
+        <p className="text-xl font-light text-gray-700">
+          Dolor tempora dolore aperiam fuga necessitatibus? Odio atque tempora
+          deleniti voluptates voluptatem. Dolorem earum voluptas blanditiis
+          labore quisquam? Quibusdam nesciunt consequatur aliquam ea corrupti
+          animi, itaque consequatur neque? At porroSit nemo aliquid quas error
+          doloremque Reiciendis ratione repellendus quae sit commodi amet
+          architecto? Aut officiis aliquam fugit nulla at necessitatibus Optio
+          totam quibusdam laboriosam aperiam libero! Officiis reiciendis Elit
+          quaerat sed vero perferendis architecto consequatur. Consequuntur ad
+          illum dolore ut accusamus. Cum possimus odit sequi quaerat beatae,
+          eveniet. Rerum dolore ipsam quia consectetur iste Veniam mollitia
+          dolores tempore? Dolor tempora dolore aperiam fuga necessitatibus?
+          Odio atque tempora deleniti voluptates voluptatem. Dolorem earum
+          voluptas blanditiis labore quisquam? Quibusdam nesciunt consequatur
+          aliquam ea corrupti animi, itaque consequatur neque? At porroSit nemo
+          aliquid quas error doloremque Reiciendis ratione repellendus quae sit
+          commodi amet architecto? Aut officiis aliquam fugit nulla at
+          necessitatibus Optio totam quibusdam laboriosam aperiam libero!
+          Officiis reiciendis Elit quaerat sed vero perferendis architecto
+          consequatur. Consequuntur ad illum dolore ut accusamus. Cum possimus
+          odit sequi quaerat beatae, eveniet. Rerum dolore ipsam quia
+          consectetur iste Veniam mollitia dolores tempore?
+        </p>
+      </div>
+      <div className="relative">
+        <div className="absolute inset-x-0 -top-28 h-32 bg-gradient-to-t from-white to-transparent pointer-events-none"></div>
+        <div className="p-4 bg-white">
+          <div className="flex space-x-4 items-center justify-center flex-col">
+            <div className="mb-4">
+              <label
+                htmlFor="weuiAgreeCheckbox"
+                className="flex items-center space-x-2"
+              >
+                <Checkbox
+                  id="weuiAgreeCheckbox"
+                  checked={checked}
+                  onChange={setChecked}
+                />
+                <span className="text-sm text-gray-600">
+                  Đọc và đồng ý với{" "}
+                  <a
+                    href="/example/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-green-500 hover:underline"
+                  >
+                    Điều khoản liên quan
+                  </a>
+                </span>
+              </label>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <Button className="py-2 px-4 rounded-md text-white bg-green-500 hover:bg-green-600">
+                阅读并同意
+              </Button>
+              <Button className="py-2 px-10 bg-gray-200 hover:bg-gray-300 rounded-md text-gray-700">
+                取消
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Button/ButtonFixedBottomPage.jsx
+++ src/pages/Button/ButtonFixedBottomPage.jsx
@@ -3,38 +3,9 @@ import ButtonBottom from "../../components/Button/ButonItem/ButtonBottom";
 
 const ButtonFixedBottomPage = () => {
   return (
-    <div className="w-full md:p-10 max-w-[100rem] mx-auto p-8 pb-2 animate-slideIn relative h-screen ">
-      <div className="w-full max-w-[100rem] mx-auto">
-        <div className="p-4 rounded-lg w-full">
-          <div className="mb-4 ">
-            Dolor tempora dolore aperiam fuga necessitatibus? Odio atque tempora
-            deleniti voluptates voluptatem. Dolorem earum voluptas blanditiis
-            labore quisquam? Quibusdam nesciunt consequatur aliquam ea corrupti
-            animi, itaque consequatur neque? At porroSit nemo aliquid quas error
-            doloremque Reiciendis ratione repellendus quae sit commodi amet
-            architecto? Aut officiis aliquam fugit nulla at necessitatibus Optio
-            totam quibusdam laboriosam aperiam libero! Officiis reiciendis Elit
-            quaerat sed vero perferendis architecto consequatur. Consequuntur ad
-            illum dolore ut accusamus. Cum possimus odit sequi quaerat beatae,
-            eveniet. Rerum dolore ipsam quia consectetur iste Veniam mollitia
-            dolores tempore? Dolor tempora dolore aperiam fuga necessitatibus?
-            Odio atque tempora deleniti voluptates voluptatem. Dolorem earum
-            voluptas blanditiis labore quisquam? Quibusdam nesciunt consequatur
-            aliquam ea corrupti animi, itaque consequatur neque? At porroSit
-            nemo aliquid quas error doloremque Reiciendis ratione repellendus
-            quae sit commodi amet architecto? Aut officiis aliquam fugit nulla
-            at necessitatibus Optio totam quibusdam laboriosam aperiam libero!
-            Officiis reiciendis Elit quaerat sed vero perferendis architecto
-            consequatur. Consequuntur ad illum dolore ut accusamus. Cum possimus
-            odit sequi quaerat beatae, eveniet. Rerum dolore ipsam quia
-            consectetur iste Veniam mollitia dolores tempore?
-          </div>
-        </div>
-      </div>
-      <div className="fixed bottom-0 left-0 w-full z-10">
-        <ButtonBottom />
-      </div>
-    </div>
+   <div className="animate-slideIn">
+    <ButtonBottom/>
+   </div>
   );
 };
 
